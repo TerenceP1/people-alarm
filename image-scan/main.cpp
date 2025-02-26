2819224387562c6a9481c6d5d625e9a1feb5cac1
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-02-26 08:10:08 -0500

    Update main.cpp

--- image-scan/main.cpp
+++ image-scan/main.cpp
@@ -535,6 +535,47 @@ public:
         res.noDelete++;
         return res;
     }
+
+    void heInit()
+    {
+        if (!isLoaded)
+        {
+            load();
+        }
+        Matrix res(rows, cols, true);
+        cl_kernel kernel = clCreateKernel(
+            program,
+            "heInit",
+            NULL);
+        clSetKernelArg(
+            kernel,
+            0,
+            sizeof(cl_mem),
+            &tBuf);
+        size_t wSz = rows * cols;
+        unsigned int twSz=wSz;
+        clSetKernelArg(
+            kernel,
+            1,
+            sizeof(unsigned int),
+            &twSz);
+        clEnqueueNDRangeKernel(
+            queue2,
+            kernel,
+            1,
+            NULL,
+            &wSz,
+            NULL,
+            0,
+            NULL,
+            NULL);
+        clFlush(queue2);
+        clFinish(queue2);
+        clReleaseKernel(kernel);
+        cout << "Pls no destructor here." << endl;
+        res.noDelete++;
+        return res;
+    }
 };
 
 string slurp(string nm)
@@ -686,7 +727,8 @@ int main(int argc, char **argv)
         a.load();
         b.load();
         cout << "Loaded!" << endl;
-        Matrix c = a.drelu();
+        Matrix c(2,2,true);
+        c.heInit();
         c.gpuPull();
         cout << "Result: ";
         for (int i = 0; i < 4; i++)
